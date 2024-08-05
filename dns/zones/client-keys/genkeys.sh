2b3a7ac765a3719a1de8697b9ffe56465b91d290
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2024-08-05 15:17:52 +0400

    remove child dirs

--- dns/zones/client-keys/genkeys.sh
+++ dns/zones/client-keys/genkeys.sh
@@ -8,13 +8,11 @@ mkdir dsset
 touch dsset/records.dsset
 for i in {0..255}
 do
-mkdir $i.orionet.re
-cd $i.orionet.re
 dnssec-keygen -a ECDSA384 -b 2048 -n ZONE $i.orionet.re
-for filename in ./*.key; do
-dnssec-dsfromkey "$filename" >> "$(pwd)/../dsset/records.dsset"
+echo "$i.orionet.re. IN NS ns0.orionet.re." >> "$(pwd)/dsset/records.dsset"
+echo "$i.orionet.re. IN NS ns1.orionet.re." >> "$(pwd)/dsset/records.dsset"
 done
-echo "$i.orionet.re. IN NS ns0.orionet.re." >> "$(pwd)/../dsset/records.dsset"
-echo "$i.orionet.re. IN NS ns1.orionet.re." >> "$(pwd)/../dsset/records.dsset"
-cd ..
+
+for filename in ./*.key; do
+dnssec-dsfromkey "$filename" >> "$(pwd)/dsset/records.dsset"
 done
