c8b68051f9740003079f8012ec5385b52b05dd27
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2024-08-05 15:11:11 +0400

    fix generator

--- dns/zones/client-keys/genkeys.sh
+++ dns/zones/client-keys/genkeys.sh
@@ -13,8 +13,8 @@ cd $i.orionet.re
 dnssec-keygen -a ECDSA384 -b 2048 -n ZONE $i.orionet.re
 for filename in ./*.key; do
 dnssec-dsfromkey "$filename" >> "$(pwd)/../dsset/records.dsset"
+done
 echo "255.orionet.re. IN NS ns0.orionet.re." >> "$(pwd)/../dsset/records.dsset"
 echo "255.orionet.re. IN NS ns1.orionet.re." >> "$(pwd)/../dsset/records.dsset"
-done
 cd ..
 done
